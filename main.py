5682db2d971b0bb395197787e5137fe7ab4dedf9
Nilson Perboni Neto <nilsonnetonapn@gmail.com>
2015-12-04 05:24:29 -0200

    restructured coded. Completing reordering, gain is low

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import random
 import sys
 import time
-from multiprocessing import Process, Array
+from multiprocessing import Process, Array, Queue
+from threading import Lock, Thread
+
+import math
 
 __author__ = 'Nilson'
 numThreads = 4
-dist = []
-pi = []
 
 """
 W = matriz de pesos (w(i,j))
@@ -28,72 +29,45 @@ se d[k-1][i][j] > d[k-1][i][k] + d[k-1][k][j], então pi[k][i][j] = pi[k-1][k][j
 
 
 def weight_serial(size):
-    # Declaring
-    myw = []
-
-    myi = []
-    for i in range(size):
-        myj = []
-        for j in range(size):
-            if i == j:
-                myj.append(0)
-            else:
-                myj.append(random.randint(2, 30))
-        myi.append(myj)
-    myw.append(myi)
-
-    return myw
-
-
-def pi_start_serial(weights, size):
-    d = []
-
     dd = []
     for i in range(size):
         ddd = []
         for j in range(size):
-            if i == j or weights[0][i][j] == sys.maxsize:
+            if i == j:
+                ddd.append(0)
+            else:
+                ddd.append(random.randint(2, 30))
+        dd.append(ddd)
+
+    return dd
+
+
+def pi_start_serial(weights, size):
+    dd = []
+    for i in range(size):
+        ddd = []
+        for j in range(size):
+            if i == j or weights[i][j] == sys.maxsize:
                 ddd.append(None)
-            elif i != j and weights[0][i][j] < sys.maxsize:
+            elif i != j and weights[i][j] < sys.maxsize:
                 ddd.append(i)
             else:
-                ddd.append(-1)  # Something is wrong
+                ddd.append(-1)
         dd.append(ddd)
-    d.append(dd)
 
-    return d
+    return dd
 
 
 def dist_serial(weights, size):
-    d = list(weights)
-    for k in range(1, size):
-        dd = []
-        for i in range(size):
-            ddd = []
-            for j in range(size):
-                if i == j:
-                    ddd.append(0)
-                else:
-                    ddd.append(int(sys.maxsize))
-            dd.append(ddd)
-        d.append(dd)
+    d = [[[(0 if __ == ___ else int(sys.maxsize)) for __ in range(size)] for __ in range(size)] for ___ in range(size)]
+    d[0] = list(weights)
+
     return d
 
 
 def pi_serial(weights, size):
-    d = list(pi_start_serial(weights, size))
-
-    for k in range(1, size):
-        dd = []
-        for i in range(size):
-            ddd = []
-            for j in range(size):
-                if i == j:
-                    ddd.append(None)
-                else:
-                    ddd.append(0)
-            dd.append(ddd)
-        d.append(dd)
+    d = [[[(None if __ == ___ else 0) for __ in range(size)] for __ in range(size)] for ___ in range(size)]
+    d[0] = list(pi_start_serial(weights, size))
 
     return d
 
@@ -105,11 +79,11 @@ def floyd_serial(weights, size):
     for k in range(1, size):
         for i in range(size):
             for j in range(size):
-                d[k][i][j] = min(d[k-1][i][j], d[k-1][i][k] + d[k-1][k][j])
-                if d[k-1][i][j] <= d[k-1][i][k] + d[k-1][k][j]:
-                    p[k][i][j] = p[k-1][i][j]
+                d[k][i][j] = min(d[k - 1][i][j], d[k - 1][i][k] + d[k - 1][k][j])
+                if d[k - 1][i][j] <= d[k - 1][i][k] + d[k - 1][k][j]:
+                    p[k][i][j] = p[k - 1][i][j]
                 else:
-                    p[k][i][j] = p[k-1][k][j]
+                    p[k][i][j] = p[k - 1][k][j]
 
     return d, p
 
@@ -119,46 +93,46 @@ def floyd_serial(weights, size):
 # ----------------------------------------------------------------------------------------------------------------- #
 # ----------------------------------------------------------------------------------------------------------------- #
 # ----------------------------------------------------------------------------------------------------------------- #
-def min_p_parallel(k, iran):
-    # print(k, i, j, d[i][k], d[k][j])]
-    '''
-    numd.append([j, min(d[i][j], d[i][k] + d[k][j])]
+def min_p_parallel(dist, pi, k, ir, size, queue):
+    darr = []
+    parr = []
+    for i in ir:
+        darr.append([(min(dist[i][j], dist[i][k] + dist[k][j])) for j in range(size)])
+        parr.append([(pi[i][j] if dist[i][j] <= dist[i][k] + dist[k][j] else pi[k][j]) for j in range(size)])
 
-    if d[i][j] <= d[i][k] + d[k][j]:
-        nump.append([j, p[i][j]])
-    else:
-        nump.append([j, p[k][j]])
-    '''
-    global dist
-    global pi
-
-    for i in iran:
-        for j in range(len(iran)*numThreads):
-            dist[k][i][j] = min(dist[k - 1][i][j], dist[k - 1][i][k] + dist[k - 1][k][j])
-            if dist[k-1][i][j] <= dist[k-1][i][k] + dist[k-1][k][j]:
-                pi[k][i][j] = pi[k - 1][i][j]
-            else:
-                pi[k][i][j] = pi[k - 1][k][j]
+    queue.put((ir, darr, parr))
 
 
 def floyd_parallel(weights, size):
-    global dist
     dist = dist_serial(weights, size)
-    global pi
     pi = pi_serial(weights, size)
 
     for k in range(1, size):
         thr = []
+        queue = Queue()
+
         for pro in range(numThreads):
-            imin = int(pro * (size/numThreads))
-            imax = int((pro + 1) * (size/numThreads))
-            iran = range(imin, imax)
-            t = Process(target=min_p_parallel, args=(k, iran))
-            t.daemon = True
+            imin = math.floor(pro * (size / numThreads))
+            imax = math.floor((pro + 1) * (size / numThreads))
+            ir = range(imin, imax)
+            t = Process(target=min_p_parallel, args=(dist[k - 1], pi[k - 1], k, ir, size, queue), daemon=True).start()
             thr.append(t)
-            t.start()
+
         for t in thr:
-            t.join()
+            try:
+                t.join()
+            except AttributeError:
+                pass
+
+        for v in range(numThreads):
+            veci, vecd, vecp = queue.get()
+            ct = 0
+            for i in veci:
+                dist[k][i] = list(vecd[ct])
+                pi[k][i] = list(vecp[ct])
+                ct += 1
+
+    return dist, pi
 
 
 if __name__ == "__main__":
@@ -167,23 +141,23 @@ if __name__ == "__main__":
     fis = open('serial.txt', 'a')
     fip = open('parallel.txt', 'a')
 
-    for n in [1*numThreads, 10*numThreads, 100*numThreads, 500*numThreads, 10000*numThreads]:
+    for n in [1 * numThreads, 10 * numThreads, 100 * numThreads]:
         w = weight_serial(n)
 
         start = time.process_time()
-        #ds, ps = floyd_serial(w, n)
+        ds, ps = floyd_serial(w, n)
         end = time.process_time()
         fis.write('For %s: Elapsed time: %ss\n' % (n, str(end-start)))
-        #print('S: For %s: Elapsed time: %ss' % (n, str(end-start)))
+        print('S: For %s: Elapsed time: %ss' % (n, str(end-start)))
 
         start = time.process_time()
-        floyd_parallel(w, n)
+        dp, pp = floyd_parallel(w, n)
         end = time.process_time()
         fip.write('For %s: Elapsed time: %ss\n' % (n, str(end-start)))
-        print('P: For %s: Elapsed time: %ss' % (n, str(end-start)))
+        print('P: For %s: Elapsed time: %ss' % (n, str(end - start)))
 
-        #print('Same dist matrix: %s' % (dist == ds))
-        #print('Same pi matrix: %s' % (pi == ps))
+        print('Same dist matrix: %s' % (dp == ds))
+        print('Same pi matrix: %s' % (pp == ps))
 
-    # print(dist)
-    # print(pi)
+        # print(dist)
+        # print(pi)
